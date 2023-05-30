87e8307f5ad3fbfc9ec3ab2236ef07fbcc67891b
João Vitor Rodrigues Piovizan <130616005+joaopiovizan@users.noreply.github.com>
2023-05-30 14:29:01 -0300

    Lista de Exercícios - Estrutura Sequencial

--- Estrutura Sequencial/ex005.cs
+++ Estrutura Sequencial/ex005.cs
@@ -1,1 +1,10 @@
-
+// Faça um Programa que converta metros para centímetros.
+        
+double metros, centimetros;
+        
+Console.WriteLine("Metros: ");
+metros = Convert.ToDouble(Console.ReadLine());
+        
+centimetros = metros * 100;
+        
+Console.WriteLine("O resultado da conversão para centrimetros é: " + centimetros);
