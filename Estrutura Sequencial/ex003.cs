e5789f4f2525ea00df0af235540a6d768b5360e7
João Vitor Rodrigues Piovizan <130616005+joaopiovizan@users.noreply.github.com>
2023-05-30 14:04:50 -0300

    Lista de exercícios - Estrutura Sequencial

---
+++ Estrutura Sequencial/ex003.cs
@@ -0,0 +1,13 @@
+// Faça um Programa que peça dois números e imprima a soma.
+
+int num1, num2, soma;
+        
+Console.WriteLine("Digite o primeiro número: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+        
+Console.WriteLine("Digite o segundo número: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+        
+soma = num1 + num2;
+        
+Console.WriteLine("A soma é: " + soma);
