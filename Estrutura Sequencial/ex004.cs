be76d6993b8ef2bd5d3240fe5555c7b582201fd5
João Vitor Rodrigues Piovizan <130616005+joaopiovizan@users.noreply.github.com>
2023-05-30 14:23:08 -0300

    Lista de Exercícios - Estrutura Sequencia

--- Estrutura Sequencial/ex004.cs
+++ Estrutura Sequencial/ex004.cs
@@ -1,1 +1,20 @@
-
+// Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+        
+double nota1, nota2, nota3, nota4;
+double media;
+        
+Console.WriteLine("Primeira nota: ");
+nota1 = Convert.ToInt32(Console.ReadLine());
+        
+Console.WriteLine("Segunda nota: ");
+nota2 = Convert.ToInt32(Console.ReadLine());
+        
+Console.WriteLine("Terceira nota: ");
+nota3 = Convert.ToInt32(Console.ReadLine());
+        
+Console.WriteLine("Quarta nota: ");
+nota4 = Convert.ToInt32(Console.ReadLine());
+        
+media = (nota1 + nota2 + nota3 + nota4) / 4;
+        
+Console.WriteLine("A média é: " + media);
