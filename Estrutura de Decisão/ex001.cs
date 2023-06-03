f1ee8f23610db634da503d5f7e870b7128901745
João Vitor R. Piovizan <joaopiovizan@gmail.com>
2023-06-03 17:08:32 -0300

    Lista de Exercícios - Estrutura de Decisão

--- Estrutura de Decisão/ex001.cs
+++ Estrutura de Decisão/ex001.cs
@@ -1,1 +1,18 @@
+// Faça um Programa que peça dois números e imprima o maior deles.
 
+int num1, num2;
+
+Console.WriteLine("Digite um número: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite outro número: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("O maior número é: " + num1);
+}
+else
+{
+    Console.WriteLine("O maior número é: " + num2);
+}
