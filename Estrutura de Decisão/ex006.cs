7ad41e39e2123773dcfba515b9b8457d39a65c61
João Vitor R. Piovizan <joaopiovizan@gmail.com>
2023-06-05 03:02:16 -0300

    Estrutura de Decisão

--- Estrutura de Decisão/ex006.cs
+++ Estrutura de Decisão/ex006.cs
@@ -1,1 +1,25 @@
 // 6. Faça um Programa que leia três números e mostre o maior deles.
+
+int num1, num2, num3;
+
+Console.WriteLine("Primeiro número: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Segundo número: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Terceiro número: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2 && num1 > num3)
+{
+    Console.WriteLine("O maior número é: " + num1);
+}
+else if (num2 > num1 && num2 > num3)
+{
+    Console.WriteLine("O maior número é: " + num2);
+}
+else
+{
+    Console.WriteLine("O maior número é: " + num3);
+}
