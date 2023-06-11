7eebc6a924bca6f6dff0a5dca3db61779824037d
João Vitor R. Piovizan <joaopiovizan@gmail.com>
2023-06-11 06:13:24 -0300

    Lista de Exercícios - Estrutura Sequencial

--- Estrutura Sequencial/ex017.cs
+++ Estrutura Sequencial/ex017.cs
@@ -5,6 +5,37 @@
     Informe ao usuário as quantidades de tinta a serem compradas e os respectivos preços em 3 situações:
     - comprar apenas latas de 18 litros;
     - comprar apenas galões de 3,6 litros;
-    - misturar latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias.
+    - misturar latas e galões, de forma que o desperdício de tinta seja menor.
+    
+    Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias.
 
 */
+
+double area, litros, litros_faltando;
+double latas, galoes;
+
+Console.WriteLine("Tamanho da área a ser pintada (m²): ");
+area = Convert.ToDouble(Console.ReadLine());
+
+litros = (area * 1.1) / 6; // acrescentados 10% de folga
+
+latas = Math.Ceiling(litros / 18);
+
+Console.WriteLine("COMPRANDO APENAS LATAS DE 18 LITROS");
+Console.WriteLine("Quantidade de latas a serem usadas: " + latas);
+Console.WriteLine("Valor total: R$ " + (latas * 80));
+
+galoes = Math.Ceiling(litros / 3.6);
+
+Console.WriteLine("\nCOMPRANDO APENAS GALÕES DE 3,6 LITROS");
+Console.WriteLine("Quantidade de galões a serem usados: " + galoes);
+Console.WriteLine("Valor total: R$ " + (galoes * 25));
+
+latas = Math.Floor(litros / 18);
+litros_faltando = litros % 18;
+galoes = Math.Ceiling(litros_faltando / 3.6);
+
+Console.WriteLine("\nMISTURANDO LATAS E GALÕES");
+Console.WriteLine("Quantidade de latas a serem usadas: " + latas);
+Console.WriteLine("Quantidade de galões a serem usados: " + galoes);
+Console.WriteLine("Valor total: R$ " + ((latas * 80) + (galoes * 25)));
