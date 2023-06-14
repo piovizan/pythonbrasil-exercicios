7a349e3ff546394b0f117e5d264b14a9235561aa
João Vitor R. Piovizan <joaopiovizan@gmail.com>
2023-06-14 08:32:52 -0300

    Lista de Exercícios - Estrutura de Decisão

--- Estrutura de Decisão/ex026.cs
+++ Estrutura de Decisão/ex026.cs
@@ -13,3 +13,46 @@
     Escreva um algoritmo que leia o número de litros vendidos, o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 2,50 o preço do litro do álcool é R$ 1,90.
 
 */
+
+double litros_vendidos, valor = 0;
+char combustivel;
+
+Console.WriteLine("Litros vendidos: ");
+litros_vendidos = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Tipo de combustível (A-álcool, G-gasolina)");
+combustivel = Convert.ToChar(Console.ReadLine().ToUpper());
+
+// fórmula: valor do desconto = preço original x (porcentagem de desconto / 100%)
+
+switch (combustivel)
+{
+    case 'A':
+        valor = litros_vendidos * 1.90;
+        
+        if (litros_vendidos <= 20)
+        {
+            valor -= (valor * 3) / 100;
+        }
+        else
+        {
+            valor -= (valor * 5) / 100;
+        }
+
+        break;
+    case 'G':
+        valor = litros_vendidos * 2.50;
+
+        if (litros_vendidos <= 20)
+        {
+            valor -= (valor * 4) / 100;
+        }
+        else
+        {
+            valor -= (valor * 6) / 100;
+        }
+
+        break;
+}
+
+Console.WriteLine("Valor a ser pago: R$ " + valor);
