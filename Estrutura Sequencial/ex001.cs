26dc23f9810c9aeee2438724a7789321af0acd95
João Vitor Rodrigues Piovizan <130616005+joaopiovizan@users.noreply.github.com>
2023-11-29 18:36:32 -0300

    Delete Estrutura Sequencial directory

--- Estrutura Sequencial/ex001.cs
+++
@@ -1,3 +0,0 @@
-// 1. Faça um Programa que mostre a mensagem "Alo mundo" na tela.
-
-Console.WriteLine("Alo mundo");
--- Estrutura Sequencial/ex002.cs
+++
@@ -1,8 +0,0 @@
-// 2. Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
-
-int numero;
-        
-Console.WriteLine("Digite um número: ");
-numero = Convert.ToInt32(Console.ReadLine());
-        
-Console.WriteLine("O número digitado foi " + numero);
