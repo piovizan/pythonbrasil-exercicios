b9737b0240fa8fb67470473dc9c9a923d60f6799
João Vitor Rodrigues Piovizan <130616005+joaopiovizan@users.noreply.github.com>
2023-11-29 18:37:03 -0300

    Delete Estrutura de Repetição directory

--- Estrutura de Repetição/ex006.cs
+++
@@ -1,6 +0,0 @@
-// 6. Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro.
-
-for (int i = 1; i <= 20; i++)
-{
-    Console.Write(i);
-}
