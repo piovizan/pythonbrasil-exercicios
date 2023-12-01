2059ec67db62144e108bbdf79aa1ac3364ac80c8
João Vitor R. Piovizan <joaopiovizan@gmail.com>
2023-12-01 19:13:17 -0300

    exercícios de estrutura de decisão

--- estruturadedecisao/exercicio003.java
+++ estruturadedecisao/exercicio003.java
@@ -1,1 +1,30 @@
 package estruturadedecisao;
+
+import java.util.Scanner;
+
+/**
+ * 03. Faça um Programa que verifique se uma letra digitada é "F" ou "M".
+ */
+public class exercicio003 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Qual o seu sexo? (f/m): ");
+        char sexo = scanner.next().charAt(0); // lendo o primeiro caractere digitado
+
+        System.out.println(sexo);
+
+        switch (sexo) {
+            case 'F':
+                System.out.println("F - Feminino");
+                break;
+            case 'M':
+                System.out.println("M - Masculino");
+                break;
+            default:
+                System.out.println("Sexo inválido");
+                break;
+        }
+    }
+}
--- estruturadedecisao/exercicio005.java
+++ estruturadedecisao/exercicio005.java
@@ -1,1 +1,31 @@
 package estruturadedecisao;
+
+import java.util.Scanner;
+
+/**
+ * 5. Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
+        - A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+        - A mensagem "Reprovado", se a média for menor do que sete;
+        - A mensagem "Aprovado com Distinção", se a média for igual a dez.
+ */
+public class exercicio005 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Primeira nota:");
+        double n1 = scanner.nextDouble();
+
+        System.out.println("Segunda nota:");
+        double n2 = scanner.nextDouble();
+
+        double media = (n1 + n2) / 2;
+
+        if (media == 10)
+            System.out.println("Aprovado com Distinção");
+        else if (media >= 7)
+            System.out.println("Aprovado");
+        else
+            System.out.println("Reprovado");
+    }
+}
