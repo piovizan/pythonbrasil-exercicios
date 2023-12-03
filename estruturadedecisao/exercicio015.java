9c61a052cb6f36fb7d7c4103e40f998ed82dc5ca
João Vitor R. Piovizan <joaopiovizan@gmail.com>
2023-12-03 18:44:29 -0300

    exercícios de estrutura de decisão

--- estruturadedecisao/exercicio015.java
+++ estruturadedecisao/exercicio015.java
@@ -1,1 +1,22 @@
 package estruturadedecisao;
+
+/**
+ * exercicio015
+ */
+public class exercicio015 {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 5;
+
+        if ((a > (b + c)) || (b > (a + c)) || (c > (a + b)))
+            System.out.println("Não pode ser um triângulo");
+        else if ((a == b) && (a == c))
+            System.out.println("É um Triângulo Equilátero");
+        else if ((a == b) || (b == c) || (a == c))
+            System.out.println("É um Triângulo Isósceles");
+        else
+            System.out.println("É um Triângulo Escaleno");
+    }
+}
